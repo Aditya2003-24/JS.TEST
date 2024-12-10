b5e1d16e986819c05362e2fc0d0eb702569c6b2b
Aditya yadav <adityayadavadi6100@gmail.com>
2024-12-10 12:08:16 +0530

    js.test

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+function multiplication() {
+  let num1 = parseInt(document.getElementById("input1").value);
+  let num2 = parseInt(document.getElementById("input2").value);
+  var result = num1 * num2;
+  document.getElementById("demo").innerHTML = result;
+}
+
+function division() {
+  let num1 = parseInt(document.getElementById("input1").value);
+  let num2 = parseInt(document.getElementById("input2").value);
+  var result = num1 * num2;
+  document.getElementById("demo").innerHTML = result;
+}
---
+++ script1.js
@@ -0,0 +1,4 @@
+
+function changColor(color){
+document.body.style.backgroundColor = color;
+}
